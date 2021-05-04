9d260ce19afd081386ea67c679981533986f65e3
Code Sandbox <hello@codesandbox.io>
2021-05-04 08:38:00 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.e89a90146b5bad60d951663b433106be.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c811b9aa87b8d7f98464b9d79fa49de",
+    "url": "https://felipeinnit.github.io/csb-dextw/index.html"
+  },
+  {
+    "revision": "8b959ee2a6658c0d0299",
+    "url": "https://felipeinnit.github.io/csb-dextw/static/js/2.235e61d4.chunk.js"
+  },
+  {
+    "revision": "0a717f6c47f2a4ff8fd4",
+    "url": "https://felipeinnit.github.io/csb-dextw/static/js/main.eea5ff6b.chunk.js"
+  },
+  {
+    "revision": "ff3e2930c3464e5b73f3",
+    "url": "https://felipeinnit.github.io/csb-dextw/static/js/runtime-main.627211c7.js"
+  }
+]);
